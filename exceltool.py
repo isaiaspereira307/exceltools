0ed1873016ac56e5a82f86ad6d707ec2e706e9a8
isaias <isaias@pop-os.localdomain>
2022-03-17 21:08:51 -0300

    v1.0

--- exceltool.py
+++ exceltool.py
@@ -31,7 +31,11 @@ def convert_file_to_csv(file_xls, file_csv): return read_file(file_xls).to_csv(f
 def remove_duplicades_lines(file): return df(file).drop_duplicates()
 
 
-def search(file, column, word): return print(df(file)[df(file)[column] == word])
+def search(list_correct, list, column, word): 
+    for index, row in df(list_correct).interrows():
+        print(row[column])
+        print(index)
+        df(list)[column].str.contains(word, regex=True)
 
 
 def clean_lines(list, column): return df(list).dropna(subset=[column], how='all')
@@ -54,10 +58,10 @@ parser = argparse.ArgumentParser(prog="exceltool", description='Excel Tool',
 parser.version = "exceltool cli 1.0.0"
 parser.add_argument('-v','--version', action="version")
 parser.add_argument('-r','--read', type=str, help='read csv')
-parser.add_argument('-c','--convert-to-csv', type=str, help='convert file to csv')
+parser.add_argument('-c','--convert-to-csv', '-convert_to_csv', type=str, help='convert file to csv')
 parser.add_argument('-cl','--clean', type=str, help='remove empty lines')
 parser.add_argument('-p','--pair', nargs=2, type=str, help='pair two csv')
-parser.add_argument('-ce','--convert-to-excel', type=str, help='convert to excel')
+parser.add_argument('-ce','--convert-to-excel','-convert_to_excel', type=str, help='convert to excel')
 parser.add_argument('-s','--search', type=str, help='search word')
 parser.add_argument('-col','--column', type=str, help='select column')
 parser.add_argument('-w','--word', type=str, help='word')
@@ -71,10 +75,10 @@ if __name__=='__main__':
             if args.pair:
                 if args.column:
                     pair_file(file1=args.pair[0], file2=args.pair[1], column=args.column).to_csv(args.output + '.csv', index=False)
-            elif args.convert-to-csv:
-                convert_file_to_csv(file_xls=str(args.convert-to-csv), file_csv=str(args.output))
-            elif args.convert-to-excel:
-                convert_to_excel(arq_csv=str(args.convert-to-excel), arq_xlsx=args.output)
+            elif args.convert_to_csv:
+                convert_file_to_csv(file_xls=str(args.convert_to_csv), file_csv=str(args.output))
+            elif args.convert_to_excel:
+                convert_to_excel(arq_csv=str(args.convert_to_excel), arq_xlsx=args.output)
             elif args.clean:
                 clean_lines(list=args.clean, column=args.column).to_csv(args.output + '.csv', index=False)
         elif args.read:
