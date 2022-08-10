02995e1bce653da59d397908c865640c24eb2a6e
isaiaspereira307 <isaias.pereira307@gmail.com>
2022-08-10 10:49:30 -0300

    refatorado

--- exceltool.py
+++ exceltool.py
@@ -5,27 +5,16 @@
 
 import pandas as pd
 import csv
-from openpyxl import (
-        Workbook,
-        load_workbook
-)
+from openpyxl import Workbook
 import argparse
-from openpyxl.styles import (
-    Font,
-    PatternFill,
-)
-import matplotlib.pyplot as plt
-from pandas.plotting import table
 
 
-# Funções Anonimas Base
 def df(arq_csv: str): return pd.read_csv(arq_csv)
 
 
 def read_file(arq_xls: str): return pd.read_excel(arq_xls)
 
 
-# Funções Anonimas
 def pair_file(file1: str, file2: str, column: str):
     return pd.merge(
             df(file1),
@@ -38,7 +27,7 @@ def pair_file(file1: str, file2: str, column: str):
     )
 
 
-def read_file(file: str): return df(file).head(50)
+def head_file(file: str): return df(file).head(50)
 
 
 def convert_file_to_csv(file_xls: str, file_csv: str):
@@ -64,7 +53,6 @@ def clean_lines(list, column):
     return df(list).dropna(subset=[column], how='all')
 
 
-# ultimo passo
 def convert_to_excel(file_csv, file_xlsx):
     wb = Workbook()
     ws = wb.active
@@ -72,75 +60,6 @@ def convert_to_excel(file_csv, file_xlsx):
         for row in csv.reader(f):
             ws.append(row)
         wb.save(file_xlsx + '.xlsx')
-
-
-# Para alterar os estilos de formatação das células
-def alterar_formatacao(
-    file_xls: str,
-    font_size: str,
-    font_name: str,
-    linha: int,
-    coluna: int,
-    valor: str,
-    negrito: bool
-):
-    wb = Workbook()
-    sheet = wb.active
-
-    sheet.cell(row=linha, column=coluna).value = valor
-    if negrito is True:
-        sheet.cell(
-            row=linha,
-            column=coluna
-        ).font = Font(size=font_size, name=font_name, bold=True)
-    else:
-        sheet.cell(
-            row=linha,
-            column=coluna
-        ).font = Font(size=font_size, name=font_name)
-
-    wb.save('styles.xlsx')
-
-
-def mudar_cores():
-    # Carregar dados para variável
-    wb = load_workbook('test.xlsx')
-    # Escolhe active sheet
-    ws = wb.active
-    # Deleta primeira coluna, que é somente índice
-    ws.delete_cols(1)
-    # Cabeçalho em negrito e fundo azul
-    # Fill parameters
-    my_fill = PatternFill(
-        start_color='5399FF',
-        end_color='5399FF',
-        fill_type='solid'
-    )
-    # Bold Parameter
-    my_font = Font(bold=True)
-    # Formata o cabeçalho
-    my_header = ['A1', 'B1', 'C1', 'D1', 'E1', 'F1']
-    for cell in my_header:
-        ws[cell].fill = my_fill
-        ws[cell].font = my_font
-        # Adiciona fórmula SUM
-    ws['F1'] = 'Total'
-    for i in range(2, 22):
-        ws['F' + str(i)] = f'=SUM(C{i}:E{i})'
-        ws['F' + str(i)].font = my_font
-        ws['F' + str(i)].fill = my_fill
-        # Salva o arquivo
-    wb.save('test.xlsx')
-
-
-def salve_in_image(image: str):
-    ax = plt.subplot(111, frame_on=False) # no visible frame
-    ax.xaxis.set_visible(False)  # hide the x axis
-    ax.yaxis.set_visible(False)  # hide the y axis
-
-    table(ax, df)  # where df is your data frame
-
-    plt.savefig(image)
 
 
 parser = argparse.ArgumentParser(
@@ -181,13 +100,12 @@ args = parser.parse_args()
 if __name__ == '__main__':
     try:
         if args.output:
-            if args.pair:
-                if args.column:
-                    pair_file(
-                        file1=args.pair[0],
-                        file2=args.pair[1],
-                        column=args.column
-                    ).to_csv(args.output + '.csv', index=False)
+            if args.pair and args.column:
+                pair_file(
+                    file1=args.pair[0],
+                    file2=args.pair[1],
+                    column=args.column
+                ).to_csv(args.output + '.csv', index=False)
             elif args.convert_to_csv:
                 convert_file_to_csv(
                     file_xls=str(args.convert_to_csv),
@@ -204,7 +122,7 @@ if __name__ == '__main__':
                     column=args.column
                 ).to_csv(args.output + '.csv', index=False)
         elif args.read:
-            print(read_file(str(args.read)))
+            print(head_file(str(args.read)))
         elif args.search:
             search(file=args.search, column=args.column, word=args.word)
 
